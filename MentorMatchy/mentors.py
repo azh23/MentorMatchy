bf5eeeb99b645e449bec0a936c1d28a0722ef0a8
Alana Zhao <zhaoam@uci.edu>
2024-05-25 13:35:46 -0700

    Various GET and POST functionalities added.

--- MentorMatchy/mentors.py
+++ MentorMatchy/mentors.py
@@ -7,55 +7,56 @@ app = Flask(__name__)
 
 # GENERIC
 
+# Retrieve users by email.
 @app.route('/<string:email>', methods=['GET'])
 def get_by_email(email: str):
     cursor.execute(
-       'SELECT * FROM USERS WHERE EMAIL_ADDRESS = (%s);',
-       {email}
+       "SELECT * FROM USERS WHERE EMAIL_ADDRESS = %s;",
+       (email,)
     )
+
+    # * ASK WHEN THEY GET TO THIS POINT: If *no* person exists on the email address, 
+    # are they ok with null being returned?
     return jsonify(cursor.fetchone()), 201
 
+# Retrieve all users.
 @app.route('/user',methods=['GET'])
 def retrieve_all():
     cursor.execute("SELECT * FROM USERS;")
     return jsonify(cursor.fetchall()), 201
 
+# Insert a user.
 @app.route('/user', methods=['POST'])
 def post_user():
     try:
         user = json.loads(request.data)
+        vals = [f"'{val}'" for val in user.values()]
+
+        cursor.execute("""
+                    INSERT INTO USERS (%s)
+                    VALUES(%s)
+                    """, 
+                    (
+                        AsIs(', '.join(user.keys())),
+                        AsIs(', '.join(vals))
+                    ))
+        database.commit()
     except Exception as e:
         return jsonify({'error': str(e)}), 400
-    
-    if not _validate_user(user):
-        return jsonify({'error': 'Invalid employee properties'}), 400
-    vals = [f"'{val}'" for val in user.values()]
-    print(', '.join(user.keys()))
-    print(', '.join(vals))
-
-    cursor.execute("""
-                    INSERT INTO USERS
-                   (%s)
-                   VALUES(%s)
-                   """, (
-                       AsIs(', '.join(user.keys())),
-                       AsIs(', '.join(vals))
-                    ))
-    database.commit()
 
     return '', 201, {'location': f'/{user['EMAIL_ADDRESS']}'}
 
-# MENTORS
-
+# Retrieve all mentors.
 @app.route('/mentors', methods=['GET'])
 def get_all_mentors():
     cursor.execute("SELECT * FROM USERS WHERE MATCHING_ROLE = 'Mentor';")
     return jsonify(cursor.fetchall()), 201
-   
-def _validate_user(mentor_info) -> bool:
-   return True
 
-# MENTEES
+# Retrieve all mentees.
+@app.route('/mentees', methods=['GET'])
+def get_all_mentees():
+    cursor.execute("SELECT * FROM USERS WHERE MATCHING_ROLE = 'Mentee';")
+    return jsonify(cursor.fetchall()), 201
 
 
 if __name__ == '__main__':
